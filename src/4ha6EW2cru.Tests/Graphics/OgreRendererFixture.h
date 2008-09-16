62dd1541a872e74624713716cb4821a9da3c80b8
nkostelnik <nkostelnik@gmail.com>
2008-09-17 00:12:47 +0100

    attempting to fix merge issues

--- src/4ha6EW2cru.Tests/Graphics/OgreRendererFixture.h
+++ src/4ha6EW2cru.Tests/Graphics/OgreRendererFixture.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/4ha6EW2cru.Tests/Graphics/OgreRendererFixture.h
 #ifndef __OGRERENDERER_FIXTURE_H
 #define __OGRERENDERER_FIXTURE_H
 
@@ -30,29 +29,3 @@ private:
 	IRenderer* _renderer;
 
 };
-
-=======
-#ifndef __OGRERENDERER_FIXTURE_H
-#define __OGRERENDERER_FIXTURE_H
-
-#include <cppunit/extensions/HelperMacros.h>
-
-class OgreRendererFixture : public CPPUNIT_NS::TestFixture
-{
-	CPPUNIT_TEST_SUITE( OgreRendererFixture );
-	CPPUNIT_TEST( Should_Intialize_Correctly );
-	CPPUNIT_TEST_SUITE_END( );
-
-public:
-
-	void setUp( );
-	void tearDown( );
-
-protected:
-
-	void Should_Intialize_Correctly( );
-
-};
-
->>>>>>> 7a510f31f1a8ff119d99ac1195a208f2b0290aa2:src/4ha6EW2cru.Tests/Graphics/OgreRendererFixture.h
-#endif
