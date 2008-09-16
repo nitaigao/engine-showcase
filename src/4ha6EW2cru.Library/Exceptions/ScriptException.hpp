2f4b47b0f8cf7cea106404e6f6f8dc25aad5de0d
nkostelnik <nkostelnik@gmail.com>
2008-09-16 09:53:12 +0100

    added an exception class

---
+++ src/4ha6EW2cru.Library/Exceptions/ScriptException.hpp
@@ -0,0 +1,23 @@
+#ifndef __SCRIPTEXCEPTION_H
+#define __SCRIPTEXCEPTION_H
+
+class ScriptException : public std::exception
+{
+
+public:
+
+	ScriptException( )
+		: std::exception( "A Script Error has occured" )
+	{
+
+	}
+
+	ScriptException( const std::string message )
+		: std::exception( message.c_str( ) )
+	{
+
+	}
+
+};
+
+#endif
